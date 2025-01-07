5f14529a42c197170cd0b497b8e3cd81932db189
TTomilin <tristan.tomilin@hotmail.com>
2025-01-07 17:33:04 +0100

    Refactoring

--- scripts/2_train_sb.py
+++ scripts/2_train_sb.py
@@ -4,16 +4,12 @@ import random
 from datetime import datetime
 from pathlib import Path
 
-import gymnasium as gym
 import numpy as np
 import wandb
-from stable_baselines3 import PPO, A2C, DQN, SAC, TD3
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.monitor import Monitor
-from wandb.integration.sb3 import WandbCallback
 
-from env.utils.utils import MultiSegmentWandBVecVideoRecorder, make_env, create_model
+from env.utils.utils import MultiSegmentVecVideoRecorder, make_env, create_model
 
 
 def main(args: argparse.Namespace):
@@ -23,13 +19,11 @@ def main(args: argparse.Namespace):
     run_name = f'{args.algorithm}_{args.env_name}_seed_{args.seed}_{timestamp}'
     base_path = Path(__file__).parent.parent.resolve()
     log_dir = f"{base_path}/logs/{run_name}"
-    wandb.init(project=args.project, mode="online", sync_tensorboard=True, name=run_name, dir=base_path, id=run_name,
-               save_code=True, group=args.env_name, job_type=args.algorithm, tags=args.wandb_tags, config=vars(args))
     os.makedirs(log_dir, exist_ok=True)
 
     env = make_vec_env(lambda: make_env(args.env_name, seed=args.seed), n_envs=args.n_envs)
     video_length = args.video_length
-    env = MultiSegmentWandBVecVideoRecorder(
+    env = MultiSegmentVecVideoRecorder(
         env,
         video_folder=f"{log_dir}/videos",
         record_video_trigger=lambda x: x % args.record_freq == 0,
@@ -48,14 +42,7 @@ def main(args: argparse.Namespace):
         deterministic=True
     )
 
-    wandb_cb = WandbCallback(
-        gradient_save_freq=args.gradient_save_freq,
-        model_save_freq=args.model_save_freq,
-        model_save_path=log_dir,
-        verbose=2
-    )
-
-    model.learn(total_timesteps=args.max_steps, callback=[eval_callback, wandb_cb])
+    model.learn(total_timesteps=args.max_steps, callback=[eval_callback])
 
     model.save(f"{log_dir}/final_model")
     wandb.save(f"{log_dir}/final_model.zip")
@@ -80,7 +67,6 @@ def parse_args():
     parser.add_argument('--gradient_save_freq', type=int, default=100, help='Frequency of saving the model')
     parser.add_argument('--record_freq', type=int, default=5000, help='Frequency of recording episodes')
     parser.add_argument('--video_length', type=int, default=1000, help='Length of the recording')
-    parser.add_argument('--wandb_tags', type=str, nargs='+', default=[], help='Tags to denote runs')
     return parser.parse_args()
 
 
